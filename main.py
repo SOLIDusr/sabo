f3da4278f8a5add076ad8ee1c9771ffb939935bb
St1zy3 <orex_80@bk.ru>
2022-12-01 21:30:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
 data_base = sqlite3.connect('bot_test.db', timeout=10)
 cursor = data_base.cursor()
 
-
+global channelid
 
 @bot.event
 async def on_ready():
+
+
+
     print('Bot launched successfully :)')
     print(f'My name is {bot.user.name}')
     print(f'My client id is {bot.user.id}')
     print('Bot Connected')
+    
     global tdict
+    tdict = {}
     await bot.add_cog(Info())
     await bot.add_cog(Gambling())
     await bot.change_presence(activity = discord.Game('r!help'))
@@ -41,6 +46,7 @@ async def on_ready():
 
 @bot.event # Узнает время в войсе
 async def on_voice_state_update(member, before, after):
+    
     author = member.id
     if before.channel is None and after.channel is not None:
         t1 = time.time()
@@ -48,7 +54,7 @@ async def on_voice_state_update(member, before, after):
     elif before.channel is not None and after.channel is None and author in tdict:
         t2 = time.time() 
         t3 = t2-tdict[author]
-        
+        global Oplata # Переменная в которой будет хранится оплаченная за команту сумма
         global vtime
         tround = math.ceil(t3)
         # Округление времени в войсе
@@ -72,8 +78,10 @@ class Info(commands.Cog):
     @commands.command()
     async def Shelp(self, ctx):
         await ctx.send('Help')
-        await ctx.send('1. "$casinohelp" - Узнать о том какие игры есть на сервере и как они работают.:leaves:')
-        await ctx.send('2. "$Valhelp" - Узнать о том что за валюта есть на данном сервере и все о ней.:leaves:')
+        await ctx.send('1. "/casinohelp" - Узнать о том какие игры есть на сервере и как они работают.:leaves:')
+        await ctx.send('2. "/Valhelp" - Узнать о том что за валюта есть на данном сервере и все о ней.:leaves:')
+        await ctx.send('3. "/rolehelp" - Узнать о покупке кастомной роли.:leaves:')
+        await ctx.send('4. "/voicehelp" - Узнать о покупке личного голосового канала.:leaves:')
 
 
     @commands.command()
@@ -96,6 +104,25 @@ class Info(commands.Cog):
         await ctx.send('"$Valhelp"')
 
 
+    @commands.command()
+    async def rolehelp(self, ctx):
+        await ctx.send('Так как на сервере есть возможность создания кастомной роли , нужно знать несколько вещей.')
+        await ctx.send('Начнем с ценника, стоимость данной услуги составляет 500.000 SH, дорого , но сделано это чтобы у каждого второго не было своей роли, это предаст обладателям кастомной роли некий эксклюзив.')
+        await ctx.send('Создание роли выполняется командой "/newrole Название роли"')
+        await ctx.send('Роль создается "Дефолтной", так что после покупки вы можете обратиться к @St1zy3 для редакции вашей роли (Цвет, значимость)')
+        await ctx.send('Под значимостью мы подразумеваем то, что роль будет находиться выше остальных и тем самым будет выделять вас.')
+
+
+    @commands.command()
+    async def voicehelp(self, ctx):
+        await ctx.send('На нашем сервере есть возможность создавать личный голосовой канал.')
+        await ctx.send('Создание происходит командой "/create_voice", стоимость создания составляет 10.000 SH.')
+        await ctx.send('Личную комнату нужно оплачивать, иначе она будет удалена.')
+        await ctx.send('После создания личной комнаты у вас будет 24 часа на оплату. Оплата недельная , стоимость 7 дней составяет 20.000 SH.')
+        await ctx.send('Оплатить комнату можно командой "/payment".')
+        await ctx.send('Хочу подчеркнуть , что пока вы (Создатель комнаты) находитесь в своей комнате , SH вы получать не будете(Данный пункт в разработке).')
+        
+
 class Gambling(commands.Cog):
     def __init__(self):
         self.bot = bot
@@ -107,6 +134,7 @@ class Gambling(commands.Cog):
         cursor = connection.cursor()
         number = random.randint(1, 100)
         jackpot = random.randint(5000, 20000)
+        
         balance = cursor.execute("SELECT money FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]# Присваиваем баланс из бд к переменной
         # Условия и т.д
         if amount is None:
@@ -208,7 +236,7 @@ async def balance(ctx):
         embed.add_field(name='Баланс:', value=f'{row[1]} SH', inline=False)
         await ctx.send(embed=embed)
 
-
+@bot.command()
 async def give_money(ctx, mention, money):
     try: 
         mention = str(mention).replace('!', '')
@@ -237,16 +265,18 @@ async def give_money(ctx, mention, money):
 #Продление войса , 20к - 7 дней
 #Оплата недельная , минимум оплатить можно на 7 дней.
 
-@bot.command()
-async def create_voice(ctx, channel_name):
 
+
+@bot.command()
+async def create_voice(ctx,channel_name):
+     
     connection = sqlite3.connect('bot_test.db')# Подключение к бд
     cursor = connection.cursor()
 
     balance = cursor.execute("SELECT money FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]# Присваиваем баланс из бд к переменной
     a = 10000
     number = 0
-    global Oplata # Переменная в которой будет хранится оплаченная за команту сумма
+    
 
     if balance < a:
         await ctx.send("Недостаточно :leaves:, иди на работу.")
@@ -257,62 +287,119 @@ async def create_voice(ctx, channel_name):
 
         guild = ctx.guild # Создание войса
         channel = await guild.create_voice_channel(channel_name)
-
+        connected = ctx.author.voice
+        channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+        await ctx.send("Голосовой канал создан.")
+        
+        channelid = channel.id
+        print(channelid)# Получает id созданного канала
+        
         #number = 1 - Голосовой чат оплачен
         #number = 2 - Не оплачен
         #number = 1
         pay = 20000 # Сумма которая будет отниматься за недельную оплату
+        await ctx.send("У вас есть 24 часа для того чтобы оплатить голосовой канал, иначе он будет удален.")
+        
 
-        def countdown(num_of_secs = 86400): #таймер, количество секунд (num_of_secs), до которого будет отсчитывать таймер (24 часа).
+        def countdown(num_of_secs = 86400): #таймер, количество секунд (num_of_secs), до которого будет отсчитывать таймер (24 часа). (86400)
             while num_of_secs: #Переменная num_of_secs будет непрерывно уменьшаться в цикле, пока не достигнет 0 (что переводится в False и завершает цикл без каких-либо дополнительных условий).
                 m,s = divmod(num_of_secs, 60) # Функция divmod принимает два числа и возвращает произведение и остаток от двух чисел.
                 min_sec_format = '{:02d}:{:02d}'.format(m, s)
-                print(min_sec_format, end='/r')
+                
                 time.sleep(1)
                 num_of_secs -= 1
 
             print('Countdown finished.')
+            global g
+            connection = sqlite3.connect('bot_test.db')# Подключение к бд
+            cursor = connection.cursor()
+            a = cursor.execute("SELECT Payment FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]# Присваиваем переменную 
+            g = int(a) 
 
             if g >= 20000: # Если в поле Payment есть 20000, войс продляется на 7 дней
                 cursor.execute("UPDATE users SET Payment = Payment - {} WHERE id = {}".format(pay, ctx.author.id))
-                def countdown(num_of_secs = 604800): #таймер, количество секунд (num_of_secs), до которого будет отсчитывать таймер (7 дней).
+                
+                def countdown1(num_of_secs = 604800): #таймер, количество секунд (num_of_secs), до которого будет отсчитывать таймер (7 дней).
                     while num_of_secs: #Переменная num_of_secs будет непрерывно уменьшаться в цикле, пока не достигнет 0 (что переводится в False и завершает цикл без каких-либо дополнительных условий).
                         m,s = divmod(num_of_secs, 60) # Функция divmod принимает два числа и возвращает произведение и остаток от двух чисел.
                         min_sec_format = '{:02d}:{:02d}'.format(m, s)
-                        print(min_sec_format, end='/r')
+                        
                         time.sleep(1)
                         num_of_secs -= 1
                     print('Countdown finished.')
-            elif g < 20000:
-                delvc() # переход в функцию удаления войса
+                  
+                countdown1()
+                    
+        countdown()    
 
 
-        async def delvc(ctx, member: discord.Member): #Должно удалять войс
-            vc = member.voice.channel
-            if vc:
-                await vc.delete()
-                await ctx.send(f"{member.name}'s voice channel - `{vc.name}` - was deleted!")
-            else:
-                await ctx.send(f"{member.name} isn't in a voice channel at the moment.")
+@bot.event 
+async def on_voice_state_update(member, before, after): #Удаляет войс 
+   
+    if g < 20000: # Если денег на Payment нет , - войс
+        cchanelid = channelid
+        channel = bot.get_channel(cchanelid)
+        await channel.delete()
+
+
+@bot.command() 
+async def newrole(ctx, *,rolename=None): 
+    connection = sqlite3.connect('bot_test.db')# Подключение к бд
+    cursor = connection.cursor()
+    rolepay = cursor.execute("SELECT money FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]# Присваиваем переменную 
+    op = int(rolepay)
+    roleprice = 500000
+    if op > 500000: # если денег хватает, идем дальше
+        if not rolename:
+            embed = discord.Embed(
+            title="Введите название роли.",
+            )
+            await ctx.send(embed=embed)
+        
+        else:
+            cursor.execute("UPDATE users SET money = money - {} WHERE id = {}".format(roleprice, ctx.author.id))
+            role = await ctx.guild.create_role(name=rolename, mentionable=True)
+            cust = role
+            await ctx.author.add_roles(role)
+            embed = discord.Embed(
+            title=f"Вы успешно создали роль. Для редакции роли обратитесь к @St1zy3"
+            )
+            await ctx.send(embed=embed)
+            customrole = {role.mention}
+
+    elif op < 500000: # Денег нехватает 
+        embed = discord.Embed(
+        title="Недостаточно средств.",
+        )
+        await ctx.send(embed=embed)
+
+    
+
 
 @bot.command() # Панель управления созданным войсом.
 async def voicemenu(ctx):
-    pass
+    await ctx.send("Команды упавления личным голосовым чатом.") 
+
+
 
 
 @bot.command()
-async def Payment(ctx, Oplata):
+async def payment(ctx, Oplata):
     # Также удалил поле time и добавил поле Payment в бд
     connection = sqlite3.connect('bot_test.db')# Подключение к бд
-    cursor = connection.cursor()      
-    global g
-    g = cursor.execute("SELECT Payment FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0] # Переменная Payment
-    balance = cursor.execute("SELECT money FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]
-    if balance >= Oplata:
-        cursor.execute("UPDATE users SET money = money - {} WHERE id = {}".format(Oplata, ctx.author.id)) # Вычитаем деньги за оплату войса
-        cursor.execute("UPDATE users SET Payment = Payment + {} WHERE id = {}".format(Oplata, ctx.author.id)) # Пополняем поле Payment
+    cursor = connection.cursor()
+    aoplata = int(Oplata)
+    balance = cursor.execute("SELECT money FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]# Присваиваем баланс из бд к переменной
+    
+    if balance >= aoplata:
+        cursor.execute("UPDATE users SET money = money - {} WHERE id = {}".format(aoplata, ctx.author.id)) # Вычитаем деньги за оплату войса
+        cursor.execute("UPDATE users SET Payment = Payment + {} WHERE id = {}".format(aoplata, ctx.author.id)) # Пополняем поле Payment
         connection.commit()
-    elif balance < Oplata:
+        embed = discord.Embed(
+        title="Оплата прошла успешно.",
+        )
+        await ctx.send(embed=embed) 
+    elif balance < aoplata:
         await ctx.send("Недостаточно средств!")
 
 
